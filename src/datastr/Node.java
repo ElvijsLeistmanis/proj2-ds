0410a8d36a6d55d3cd0074217b371c47c6374cb4
Elvijs <s22leiselvi@venta.lv>
2026-03-31 11:01:20 +0300

    Added Node class

---
+++ src/datastr/Node.java
@@ -0,0 +1,35 @@
+package datastr;
+
+public class Node<T> {
+	private T element;
+	private Node<T> next = null;
+	private Node<T> previous = null;
+	
+	public T getElement() {
+		return element;
+	}
+	public Node<T> getNext() {
+		return next;
+	}
+	public Node<T> getPrevious() {
+		return previous;
+	}
+	
+	public void setElement(T element) {
+		if(element != null) {
+			this.element = element;
+			return;
+		}
+		this.element = (T)new Object();
+	}
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+	public void setPrevious(Node<T> previous) {
+		this.previous = previous;
+	}
+	
+	public Node(T element) {
+		setElement(element);
+	}
+}
